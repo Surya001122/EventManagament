bc4557ee79831f28d72a2bf2bd5f84df691f20d2
Surya prakash R <suryareigns001@gmail.com>
2022-11-07 18:14:16 +0530

    Holiday and birthday Updates

--- src/Admin.java
+++ src/Admin.java
@@ -22,5 +22,7 @@ public class Admin {
 
     public void addPublicHolidays(){} // admin can add holidays manually before creating the calendar instance for each user.
     public void removeHolidays(){} // admin can remove holidays
-    public static String getBirthDate(String phoneNumber){return null;} //  admin checks the users list to check whether the phoneNumber sent by the user matches with the users phoneNumber in the list and returns
+    public static void setBirthdayEvent(String phoneNumber){
+
+    }
 }
