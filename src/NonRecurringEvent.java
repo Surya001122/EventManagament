f925be8fbc235f2fb8cc8146e71855f426b0601d
Surya prakash R <suryareigns001@gmail.com>
2022-11-11 13:10:58 +0530

    initial commit

--- src/NonRecurringEvent.java
+++ src/NonRecurringEvent.java
@@ -1,6 +1,6 @@
 public class NonRecurringEvent extends Event{
-    private String eventStartTime;
-    private String eventEndTime;
+    private String nonRecurringEventStartTime; // start time varies according to the user.
+    private String nonRecurringEventEndTime; // end time varies according to the user.
     Notify normalNotifier;
     private int delay;
 
--- src/RecurringEvent.java
+++ src/RecurringEvent.java
@@ -1,7 +1,12 @@
 public class RecurringEvent extends Event{
+    private final String RecurringEventStartTime = ""; // start time of holiday or birthday starts at 00:00 on that day.
+    private final String RecurringEventEndTime = ""; // end time of holiday or birthday starts at 24:00 on that day.
     Notify recurringNotifier;
     private int delay;
     private int period;
+
+    public RecurringEvent() {
+    }
 
     @Override
     public void createReminder(){}
